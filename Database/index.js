1ce64373f755ea9c9025c9e133375d1abf6e0c73
Maxwell Philip <max.philip1@gmail.com>
2022-11-01 11:40:00 -0400

    remove unnecessary files

--- Database/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
-
-let someSchema = mongoose.Schema({
-  // TODO: your schema here!
-});
-
-let Repo = mongoose.model('Repo', someSchema);
-  //creates a model based of the schema provided
